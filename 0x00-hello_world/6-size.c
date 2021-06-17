18f9f7cb74c8c0fdb8e5418dfc076eb45813e681
EngrJoshN <joshuanyamweno@yahoo.com>
2021-06-17 14:49:17 -0700

    Size of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ float floatType;
 char charType;
 long int longType;
 long long int doubleType;
- fprintf(stderr, "Size of a char: %zu byte(s)\n", sizeof(charType));
- fprintf(stderr, "Size of an int: %zu byte(s)\n", sizeof(intType));
- fprintf(stderr, "Size of a long int: zu byte(s)\n", sizeof(longType));
- fprintf(stderr, "Size of a long long int: %zu byte(s)\n", sizeof(doubleType));
- fprintf(stderr, "Size of a float: %zu byte(s)\n", sizeof(floatType));
+fprintf(stderr, "Size of a char: %zu byte(s)\n", sizeof(charType));
+fprintf(stderr, "Size of an int: %zu byte(s)\n", sizeof(intType));
+fprintf(stderr, "Size of a long int: %zu byte(s)\n", sizeof(longType));
+fprintf(stderr, "Size of a long long int: %zu byte(s)\n", sizeof(doubleType));
+fprintf(stderr, "Size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
