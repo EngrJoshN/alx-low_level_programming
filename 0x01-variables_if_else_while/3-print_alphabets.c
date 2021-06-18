3477c82e1ee3b64313a43fce91d073fae3c8621a
EngrJoshN <joshuanyamweno@yahoo.com>
2021-06-18 06:40:00 -0700

    Print alphabet +- caps

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,24 +1,24 @@
-#inclde <stdio.h>
+#include <stdio.h>
 
 /**
-*print alphabet in lower case and un uppercase 'a-z' using putchar
-*Return: Always 0 (Success)
+* main - print alphabet in lower case and un uppercase 'a-z' using putchar
+* Return: Always 0 (Success)
 */
 
 int main(void)
 {
 char lower = 'a';
 char upper = 'A';
- 
+
 while (lower <= 'z')/* print lowercase a-z*/
- 
+
 {
 putchar(lower);
 lower++;
 }
 
 while (Upper <= 'Z')/* print uppercase A-Z*/
- 
+
 {
 putchar(upper);
 Upper++;
