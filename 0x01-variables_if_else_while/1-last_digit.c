db9a8a15f44b97c60e7e8f60c1e240d400f68372
EngrJoshN <joshuanyamweno@yahoo.com>
2021-06-18 05:30:48 -0700

    last gigit of a number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,19 +1,19 @@
 #include <stdlib.h>
 #include <time.h>
-#inclde <stdio.h>
+#include <stdio.h>
 
 /**
-*main -  Entry point
-*Return: Always 0 (Success)
+* main -  Entry point
+* Return: Always 0 (Success)
 */
 
 int main(void)
 {
 int n;
-
+ 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
- 
+
 printf("Last digit of %i is %i and is", n, (n % 10));
 
 if ((n % 10) == 0)
