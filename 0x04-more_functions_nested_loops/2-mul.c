5304b16436775b4a45dbbc31afb7c81b44a1d55a
EngrJoshN <joshuanyamweno@yahoo.com>
2021-06-28 12:58:06 -0700

    Pointers arrays and strings

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,19 @@
 #include "holberton.h"
 /**
- * multiply two numbers
- * return int.
+ * multiply two numbers 
+ * @mul: multiply
+ * @a: first number
+ * @b: second number
+ * return int c
  */
+
 int mul(int a, int b)
+
 {
   int c;
+
   c = a * b;
-  
-  return(c);
-   
- }
+
+  return (c);
+
+}
