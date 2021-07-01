ec2a5cbbb2929966eff63465f30bd2e1b65b4f49
EngrJoshN <joshuanyamweno@yahoo.com>
2021-07-01 03:56:24 -0700

    Append src string to dest

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,22 @@
-#include <stdio.h>
+#include "holberton.h"
 /**
  * _strncat -  concatenates two strings.
- * @src: array with the string source
- * @dest: pointer where the text is append
- * @n: num of bytes to be append from src
- *
- * Return: Always 0.
+ * @src: Pointer to the source string
+ * @dest: pointer to the destination string
+ * @n: number of bytes to print
+ * Return: Pointer to the resulting string dest.
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int c_src;
-int c_dest;
-int i_src;
-int i_dest;
+int i, j;
+i = 0;
 
-for (c_dest = 0; dest[c_dest] != 0; c_dest++)
-for (c_src = 0; c_src < n && src[c_src] != 0; c_src++)
-i_src = 0;
-for (i_dest = c_dest; i_dest < (c_src + c_dest); i_dest++)
+for (j = 0; dest[j] != '\0'; j++)
+;
+while (src[i] != '\0' && i < n)
 {
-dest[i_dest] = src[i_src];
-i_src++;
+dest[j + i] = src[i];
+i++;
 }
-dest[c_src + c_dest] = 0;
 return (dest);
 }
