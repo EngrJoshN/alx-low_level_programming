7a07a2a7e4f0f796e8892a9989ba133920721d4d
EngrJoshN <joshuanyamweno@yahoo.com>
2021-07-01 03:40:11 -0700

    Concatenate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,23 +4,19 @@
  * @src: array with the string source
  * @dest: pointer where the text is append
  *
- * Return: Always 0.
+ * Return: Pointer to the resulting string dest.
  */
 char *_strcat(char *dest, char *src)
 {
-int c_src;
-int c_dest;
-int i_src;
-int i_dest;
+int i, j;
+i = 0;
 
-for (c_dest = 0; dest[c_dest] != 0; c_dest++)
-for (c_src = 0; src[c_src] != 0; c_src++)
-i_src = 0;
-for (i_dest = c_dest; i_dest < (c_src + c_dest); i_dest++)
+for (j = 0; dest[j] != '\0'; j++)
+;
+while (src[i] != '\0')
 {
-dest[i_dest] = src[i_src];
-i_src++;
+dest[j + i] = src[i];
+i++;
 }
-dest[c_src + c_dest] = 0;
 return (dest);
 }
